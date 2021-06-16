1c177ec82ff11d05b1882b2bb5af42302f10a4b6
freshmea <freshmea@naver.com>
2021-06-17 00:42:21 +0900

    서보 모터

--- main.py
+++ main.py
@@ -14,12 +14,26 @@ def blink(my_board, pin):
         print('off')
         my_board.digital_write(pin, 0)
         time.sleep(1)
-    my_board.shutdown()
+    # my_board.shutdown()
 
 board = pymata4.Pymata4()
 
+
+def servo(my_board, pin):
+
+    my_board.set_pin_mode_servo(pin)
+    for _ in range(5):
+        for i in range(80):
+            my_board.servo_write(pin, i*2)
+            time.sleep(0.1)
+            # my_board.servo_write(pin, 90)
+            # time.sleep(0.1)
+            # my_board.servo_write(pin, 180)
+
 try:
     blink(board, DIGITAL_PIN)
+    time.sleep(1)
+    servo(board, 9)
 except:
     board.shutdown()
     sys.exit(0)
