c21cd00af1cce2f5ad63bb5eae15eb2a5272ede8
freshmea <freshmea@naver.com>
2022-07-16 14:32:47 +0900

    buzzer2

--- buzzerMelodyGenerator(BMG)/test.c
+++ buzzerMelodyGenerator(BMG)/test.c
@@ -1,8 +1,214 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
+
+#define B0	31
+#define C1	33
+#define CS1	35
+#define D1	37
+#define E1	41
+#define F1	44
+#define FS1	46
+#define G1	49
+#define GS1	52
+#define A1	55
+#define AS1	58
+#define B1	62
+#define C2	65
+#define CS2	69
+#define D2	73
+#define DS2	78
+#define E2	82
+#define F2	87
+#define FS2	93
+#define G2	98
+#define GS2	104
+#define A2	110
+#define AS2	117
+#define B2	123
+#define C3	131
+#define CS3	139
+#define D3	147
+#define DS3	156
+#define E3	165
+#define F3	175
+#define FS3	185
+#define G3	196
+#define GS3	208
+#define A3	220
+#define AS3	233
+#define B3	247
+#define C4	262
+#define CS4	277
+#define D4	294
+#define DS4	311
+#define E4	330
+#define F4	349
+#define FS4	370
+#define G4	392
+#define GS4	415
+#define A4	440
+#define AS4	466
+#define B4	494
+#define C5	523
+#define CS5	554
+#define D5	587
+#define DS5	622
+#define E5	659
+#define F5	698
+#define FS5	740
+#define G5	784
+#define GS5	831
+#define A5	880
+#define AS5	932
+#define B5	988
+#define C6	1047
+#define CS6	1109
+#define D6	1175
+#define DS6	1245
+#define E6	1319
+#define F6	1397
+#define FS6	1480
+#define G6	1568
+#define GS6	1661
+#define A6	1760
+#define AS6	1865
+#define B6	1976
+#define C7	2093
+#define CS7	2217
+#define D7	2349
+#define DS7	2489
+#define E7	2637
+#define F7	2794
+#define FS7	2960
+#define G7	3136
+#define GS7	3322
+#define A7	3520
+#define AS7	3729
+#define B7	3951
+#define C8	4186
+#define CS8	4435
+#define D8	4699
+#define DS8	4978
+#define E8	5274
+#define F8	5588
+#define FS8	5920
+#define G8	6272
+#define GS8	6645
+#define A8	7040
+#define AS8	7459
+#define B8	7902
+#define REST	0
 
 int main(void)
 {
-	printf("df");
-	Beep(1567, 500);
+	// char inc;
+	while(1){
+			if(kbhit()){
+				printf("%d", kbhit());
+			}
+			Beep(G8, 500);
+		}
+
+	return 0;
 }
+C1	33
+CS1	35
+D1	37
+DS1	39
+E1	41
+F1	44
+FS1	46
+G1	49
+GS1	52
+A1	55
+AS1	58
+B1	62
+C2	65
+CS2	69
+D2	73
+DS2	78
+E2	82
+F2	87
+FS2	93
+G2	98
+GS2	104
+A2	110
+AS2	117
+B2	123
+C3	131
+CS3	139
+D3	147
+DS3	156
+E3	165
+F3	175
+FS3	185
+G3	196
+GS3	208
+A3	220
+AS3	233
+B3	247
+C4	262
+CS4	277
+D4	294
+DS4	311
+E4	330
+F4	349
+FS4	370
+G4	392
+GS4	415
+A4	440
+AS4	466
+B4	494
+C5	523
+CS5	554
+D5	587
+DS5	622
+E5	659
+F5	698
+FS5	740
+G5	784
+GS5	831
+A5	880
+AS5	932
+B5	988
+C6	1047
+CS6	1109
+D6	1175
+DS6	1245
+E6	1319
+F6	1397
+FS6	1480
+G6	1568
+GS6	1661
+A6	1760
+AS6	1865
+B6	1976
+C7	2093
+CS7	2217
+D7	2349
+DS7	2489
+E7	2637
+F7	2794
+FS7	2960
+G7	3136
+GS7	3322
+A7	3520
+AS7	3729
+B7	3951
+C8	4186
+CS8	4435
+D8	4699
+DS8	4978
+E8	5274
+F8	5588
+FS8	5920
+G8	6272
+GS8	6645
+A8	7040
+AS8	7459
+B8	7902
+0	0
+0	0
+0	0
+0	0
